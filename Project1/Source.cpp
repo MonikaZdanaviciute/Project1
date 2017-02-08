37b7b59ee451773b3765395c12f7b15330a0b9ee
if_user <if_user@VDUIF-501-16>
2017-02-08 11:05:49 +0200

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+void nuskaitymas()
+{
+	ifstream D;
+	D.open("masinos.txt");
+	cout << "Failas atidarytas " << endl;
+	char eil1;
+	while (!D.eof())
+	{
+		D.get(eil1);
+		cout << eil1;
+	}
+	D.close();
+
+	void spalvosradmas(;)
+{
+	cout << "iveskite spalva" << endl;
+	cin >> spalva;
+	for(int i=0; i<masinusk; i++)
+	{
+if
+}
+
+
+
+
+
+
+
+
+
+
+void main()char spalva;
